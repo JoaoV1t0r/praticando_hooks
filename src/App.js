6ed58b6d4dda674e76421055d502dfe21edf6b0b
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-07-06 22:13:32 -0300

    add increment

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import { useState } from 'react';
 function App() {
   const [reverse, setReverse] = useState(false);
   const reverseClass = reverse ? 'reverse' : '';
+  const [counter, setCounter] = useState(0);
 
   const handlerClick = () => {
     setReverse(!reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
   };
 
   return (
@@ -15,7 +20,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
         <h1>Contador : 0</h1>
-        <button type="button" onClick={handlerClick()}>
+        <button type="button" onClick={handleIncrement}>
+          Increment
+        </button>
+        <button type="button" onClick={handlerClick}>
           Reverse
         </button>
       </header>
