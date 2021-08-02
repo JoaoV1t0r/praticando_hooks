38a2dbabe9c0847083e4d883cc73200f1fcd99ba
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-08-02 20:39:26 -0300

    usando useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { Div } from './components/Div';
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/App';
+
+const globalState = {
+  title: 'Esse é o título',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+// eslint-disable-next-line
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda':
+      return { ...state, title: 'Title Mudou' };
+    case 'conta': {
+      const { counter } = state;
+      return { ...state, counter: counter + 1 };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  // eslint-disable-next-line
+  const [state, dispatch] = useReducer(reducer, globalState);
+  // eslint-disable-next-line
+  const { title, body, counter } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} | {counter}
+      </h1>
+      <button
+        onClick={() => {
+          dispatch({ type: 'muda' });
+        }}
+      >
+        Click
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'conta' });
+        }}
+      >
+        Conta
+      </button>
+    </div>
   );
 }
 export default App;
