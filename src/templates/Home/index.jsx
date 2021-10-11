10abe9895ee5380919b217c7b2c38ddfd3d012fd
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-10-11 20:40:23 -0300

    using hook useDebugValue and create useMediaQuery

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,49 +1,40 @@
-import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
+import { useDebugValue, useEffect, useState } from 'react';
 
-export const Home = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
-  const divRef = useRef();
+const useMediaQuery = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(initialValue);
 
-  const handleClick = () => {
-    setCounted((c) => [...c, +c.slice(-1) + 1]);
-    divRef.current.handleClick();
-  };
-
-  useLayoutEffect(() => {
-    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
+  useDebugValue(`Query: ${queryValue}`, (name) => {
+    return name + ' modificado';
   });
 
-  return (
-    <>
-      <button onClick={handleClick}>Counted {counted.slice(-1)}</button>
-      <DisplayCounted counted={counted} ref={divRef} />
-    </>
-  );
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
 };
 
-export const DisplayCounted = forwardRef(function DisplayCounted({ counted }, ref) {
-  const [rand, setRant] = useState('0.24');
-  const divRef = useRef();
+export const Home = () => {
+  const huge = useMediaQuery('(min-width: 980px)');
+  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
+  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
+  const small = useMediaQuery('(max-width: 321px)');
 
-  const handleClick = () => {
-    setRant(Math.random().toFixed(2));
-  };
+  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'purple' : null;
 
-  useImperativeHandle(ref, () => ({
-    handleClick,
-    divRef: divRef.current,
-  }));
-
-  return (
-    <div ref={divRef} style={{ height: '400px', width: '300px', overflow: 'scroll' }}>
-      {counted.map((count, index) => {
-        return (
-          <p onClick={handleClick} key={`count-${index}`}>
-            {count} +++ {rand}
-          </p>
-        );
-      })}
-    </div>
-  );
-});
+  return <div style={{ fontSize: '60px', background }}>Oi</div>;
+};
