0c2111d1e4d508aba7464fea6b60784f423c092f
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-10-11 20:56:02 -0300

    using Children of React

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,40 +1,22 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { Children, cloneElement } from 'react';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
 
-  useDebugValue(`Query: ${queryValue}`, (name) => {
-    return name + ' modificado';
+const Parent = ({ children }) => {
+  return Children.map(children, (child) => {
+    const newChild = cloneElement(child, { ...s });
+    return newChild;
   });
-
-  useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
-
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
 };
 
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
-  const small = useMediaQuery('(max-width: 321px)');
-
-  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'purple' : null;
-
-  return <div style={{ fontSize: '60px', background }}>Oi</div>;
+  return (
+    <Parent>
+      <p>Oi</p>
+    </Parent>
+  );
 };
