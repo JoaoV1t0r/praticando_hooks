183263f62bed8af3828f8269de94bdf9c4d34307
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-10-11 21:16:06 -0300

    adding context

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,22 +1,49 @@
-import { Children, cloneElement } from 'react';
+import { Children, cloneElement, createContext, useContext, useState } from 'react';
 
 const s = {
   style: {
-    fontSize: '60px',
+    fontSize: '40px',
   },
 };
 
-const Parent = ({ children }) => {
-  return Children.map(children, (child) => {
-    const newChild = cloneElement(child, { ...s });
-    return newChild;
-  });
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
 };
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
+const P = ({ children }) => <p {...s}>{children}</p>;
 
 export const Home = () => {
   return (
-    <Parent>
-      <p>Oi</p>
-    </Parent>
+    <TurnOnOff>
+      <div>
+        <TurnedOn>
+          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+        </TurnedOn>
+        <TurnedOff>
+          <P>Aqui vem as coisas do OFF.</P>
+        </TurnedOff>
+        <TurnButton {...s} />
+      </div>
+    </TurnOnOff>
   );
 };
