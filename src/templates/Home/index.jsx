5ef9dc356289381d97641211bbe973e369ff050f
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-10-11 21:31:44 -0300

    using lazy loading

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,49 +1,20 @@
-import { Children, cloneElement, createContext, useContext, useState } from 'react';
+import React, { useState } from 'react';
+import { Suspense } from 'react';
+// import LazyComponent from './laze-component';7
 
-const s = {
-  style: {
-    fontSize: '40px',
-  },
-};
-
-const TurnOnOffContext = createContext();
-
-const TurnOnOff = ({ children }) => {
-  const [isOn, setIsOn] = useState(false);
-  const onTurn = () => setIsOn((s) => !s);
-
-  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
-};
-const TurnedOn = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? children : null;
-};
-const TurnedOff = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? null : children;
-};
-const TurnButton = ({ ...props }) => {
-  const { isOn, onTurn } = useContext(TurnOnOffContext);
-  return (
-    <button onClick={onTurn} {...props}>
-      Turn {isOn ? 'OFF' : 'ON'}
-    </button>
-  );
-};
-const P = ({ children }) => <p {...s}>{children}</p>;
+const loadComponent = () => import('./laze-component');
+const LazyComponent = React.lazy(loadComponent);
 
 export const Home = () => {
+  const [show, setShow] = useState(false);
   return (
-    <TurnOnOff>
-      <div>
-        <TurnedOn>
-          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
-        </TurnedOn>
-        <TurnedOff>
-          <P>Aqui vem as coisas do OFF.</P>
-        </TurnedOff>
-        <TurnButton {...s} />
-      </div>
-    </TurnOnOff>
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          {show ? 'Show' : 'Show Off'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
   );
 };
---
+++ src/templates/Home/laze-component.jsx
@@ -0,0 +1,9 @@
+const LazyComponent = () => {
+  return (
+    <div>
+      <p>Oi</p>
+    </div>
+  );
+};
+
+export default LazyComponent;
